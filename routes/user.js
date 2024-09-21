9877278d6a93fa509e99e1e667e3bb763265291a
SARTHAKtyag <st957879@gmail.com>
2024-09-22 01:19:32 +0530

    update

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const usercontroller=require('../controllers/usercontroller')
+const router=express.Router();
+router.get('/users',(req,res)=>{
+    usercontroller.getUser(req,res);
+})
+router.get('/user/:id',(req,res)=>{
+usercontroller.getParticularUser(req,res)
+})
+module.exports=router;
